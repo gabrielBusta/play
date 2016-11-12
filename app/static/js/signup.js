43e3e6bfdc40a9c8ac49919ae43374ac35f503ca
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-12 17:51:53 -0600

    added signup script

---
+++ app/static/js/signup.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+    $("#sign-up-form").on("submit", function(e) {
+        event.preventDefault();
+        console.log("form submitted.");
+        sendSignUpPost();
+    });
+
+    function sendSignUpPost() {
+        $.ajax({
+            url: "/",
+            type: "POST",
+            data: {
+                username: $("#username-input").val(),
+                first_name: $("#first-name-input").val(),
+                last_name: $("#last-name-input").val(),
+                email: $("#email-input").val(),
+                password1: $("#password1-input").val(),
+                password2: $("#password2-input").val()
+            },
+            success: function(reponse) {
+                // clearSignUpForm();
+                console.log("success.");
+            },
+            error: function(xhr, errmsg, err) {
+                alert("oops! We have encountered an error: " + errmsg);
+                // provide a bit more info about the error to the console
+                console.log(xhr.status + ": " + xhr.responseText);
+            }
+        });
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                var csrftoken = getCookie("csrftoken");
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+    // This function gets cookie with a given name
+    function getCookie(name) {
+        var cookieValue = null;
+
+        if (document.cookie && document.cookie != "") {
+            var cookies = document.cookie.split(";");
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + "=")) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    /**
+     * The functions below will create a header with csrftoken
+     */
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    function sameOrigin(url) {
+        // test that a given url is a same-origin URL
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = "//" + host;
+        var origin = protocol + sr_origin;
+
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + "/") ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + "/") ||
+            // or any other URL that isn"t scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+});

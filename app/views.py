1b211a807afd02dd8b8e61832b599126d411dfad
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-12 17:16:35 -0600

    added signup screen

--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'app/login.html', {})
+
+
+def signup(request):
+    return render(request, 'app/signup.html', {})

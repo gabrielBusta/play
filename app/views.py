54d02d3c7c2c78abeba08f207e83dddd7cb82484
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-12 20:22:59 -0600

    added email validation

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.core.validators import validate_email
 
 def login(request):
     return render(request, 'app/login.html', {})
@@ -7,17 +8,22 @@ def login(request):
 
 def signup(request):
     if request.method == 'POST':
-        first_name=request.POST.get('first_name')
-        last_name=request.POST.get('last_name')
-        email=request.POST.get('email')
-        username=request.POST.get('username')
-        
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        try:
+            email = request.POST.get('email')
+            validate_email(email)
+        except forms.ValidationError:
+            print("error validating form")
+
+        username = request.POST.get('username')
+
         # TODO: add password validation
         if request.POST.get('password1') == request.POST.get('password2'):
-            password=request.POST.get('password1')
-        
-        user= User.objects.create_user(username,email,password)
+            password = request.POST.get('password1')
+
+        user = User.objects.create_user(username,email,password)
         user.first_name=first_name
         user.last_name=last_name
-        
+
     return render(request, 'app/signup.html', {})

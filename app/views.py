4574283b1b36a0dd3171d94f6bff22d4e9f5c7fc
Abby <abbyyy23@gmail.com>
2016-11-12 19:41:10 -0600

    added signup on the server

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'app/login.html', {})
@@ -7,5 +7,17 @@ def login(request):
 
 def signup(request):
     if request.method == 'POST':
-        pass
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        email=request.POST.get('email')
+        username=request.POST.get('username')
+        
+        # TODO: add password validation
+        if request.POST.get('password1') == request.POST.get('password2'):
+            password=request.POST.get('password1')
+        
+        user= User.objects.create_user(username,email,password)
+        user.first_name=first_name
+        user.last_name=last_name
+        
     return render(request, 'app/signup.html', {})

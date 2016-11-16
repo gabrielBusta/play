6c44cfb3ab125b4dd0ddd5ebdbdcbf8f98ccdde6
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-15 19:32:16 -0600

    added fields to models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class Country(models.Model):
     primary_language = models.CharField(max_length=20)
     country_code = models.CharField(max_length=3)
 
+
 class Artist(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -20,15 +21,29 @@ class Artist(models.Model):
 
 
 class Track(models.Model):
-    pass
+    # TODO: ADD FILE PATH
+    name = models.CharField(max_length=50)
+    duration = models.DurationField()
+    # TODO: ADD Artist AND Release ForeignKey
 
 
 class Release(models.Model):
-    pass
+    label = models.ForeignKey(Label)
+    # ADD Artist RELATIONSHIP
+    country = models.ForeignKey(Country)
+    name = models.CharField(max_length=50)
+    release_date = models.DateField()
+    release_type = models.CharField(max_length=1, choices=(('ALBUM', 'EP', 'SINGLE'))
+    # TODO: ADD LIST OF GENRES
+    language = models.CharField(max_length=20)
 
 
 class Label(models.Model):
-    pass
+    country = models.ForeignKey(Country)
+    name = models.CharField(max_length=50)
+    founded_date = models.DateField()
+    dissolved_date = models.DateField()
+    dissolved = models.BooleanField(default=False)
 
 
 class Playlist(models.Model):

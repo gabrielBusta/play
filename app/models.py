5ab8de54819860cf56d5154cec5f8bee6d6717cb
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-15 23:02:51 -0600

    got the bug, changed the formatting according to docs

--- app/models.py
+++ app/models.py
@@ -13,9 +13,17 @@ class Artist(models.Model):
     begin_date = models.DateField()
     end_date = models.DateField()
     ended = models.BooleanField(default=False)
-    gender = models.CharField(max_length=1, choices=(
-             ('M', 'F'),
-             ('m', 'f')))
+
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+    GENDER_CHOICES = ((MALE, 'Male'),
+                      (FEMALE, 'Female'),
+                      (OTHER, 'Other'))
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
     bio = models.CharField(max_length=500)
     country = models.ForeignKey(Country)
 
@@ -27,23 +35,33 @@ class Track(models.Model):
     # TODO: ADD Artist AND Release ForeignKey
 
 
-class Release(models.Model):
-    label = models.ForeignKey(Label)
-    # ADD Artist RELATIONSHIP
-    country = models.ForeignKey(Country)
-    name = models.CharField(max_length=50)
-    release_date = models.DateField()
-    release_type = models.CharField(max_length=1, choices=(('ALBUM', 'EP', 'SINGLE'))
-    # TODO: ADD LIST OF GENRES
-    language = models.CharField(max_length=20)
-
-
 class Label(models.Model):
     country = models.ForeignKey(Country)
     name = models.CharField(max_length=50)
     founded_date = models.DateField()
     dissolved_date = models.DateField()
     dissolved = models.BooleanField(default=False)
+
+
+class Release(models.Model):
+    label = models.ForeignKey(Label)
+    # TODO: ADD Artist RELATIONSHIP
+    country = models.ForeignKey(Country)
+    name = models.CharField(max_length=50)
+    release_date = models.DateField()
+
+    ALBUM = 'AL'
+    SINGLE = 'SI'
+    EXTENDED_PLAY = 'EP'
+
+    RELEASE_TYPE_CHOICES = ((ALBUM, 'Album'),
+                            (SINGLE, 'Single'),
+                            (EXTENDED_PLAY, 'EP'))
+
+    release_type = models.CharField(max_length=2, choices=RELEASE_TYPE_CHOICES)
+
+    # TODO: ADD LIST OF GENRES
+    language = models.CharField(max_length=20)
 
 
 class Playlist(models.Model):

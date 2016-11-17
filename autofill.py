d12e0ed228350699517abcb3cb416816fee6d9e8
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-17 00:15:09 -0600

    got 25 artist

--- autofill.py
+++ autofill.py
@@ -1,21 +1,35 @@
+import os
+import sys
+import musicbrainzngs as mbz
+
 def main():
     mbz.set_useragent("play music app", "0.1", "gabrielbusta@gmail.com")
 
-    result = mbz.search_releases(artist="Drake", release="Views", limit=1)
+    result = mbz.search_artists(type="group")\
 
     for key in result.keys():
         print(key)
-        print(result[key])
-        print()
+
+    print("number of artist:" + str(len(result["artist-list"])))
+    print("artist-list = ")
+    uprint(result["artist-list"])
+    # uprint(result)
 
     models.Country.objects.all()
 
 
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
+
+
 if __name__ == '__main__':
-    import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     import django
     django.setup()
     from app import models
-    import musicbrainzngs as mbz
     main()

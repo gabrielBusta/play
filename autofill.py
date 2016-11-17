f29518478c62354bdc66695faa141f8d4e1984df
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-16 23:18:52 -0600

    completed setup

--- autofill.py
+++ autofill.py
@@ -1,19 +1,17 @@
-import django
-import os
-import musicbrainzngs as mbz
-
-from app import models
-
 def main():
     mbz.set_useragent("play music app", "0.1", "gabrielbusta@gmail.com")
 
     result = mbz.search_releases(artist="Drake", release="Views", limit=1)
     print(result)
-    
+
     models.Country.objects.all()
 
 
 if __name__ == '__main__':
+    import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    import django
     django.setup()
+    from app import models
+    import musicbrainzngs as mbz
     main()

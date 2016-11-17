e7e2fc087b2d5493009a5907822b25e2d3446ec6
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-16 23:14:42 -0600

    imported musicbrainz:

--- autofill.py
+++ autofill.py
@@ -1,9 +1,19 @@
 import django
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-django.setup()
+import musicbrainzngs as mbz
 
 from app import models
 
-models.Country.objects.all()
+def main():
+    mbz.set_useragent("play music app", "0.1", "gabrielbusta@gmail.com")
+
+    result = mbz.search_releases(artist="Drake", release="Views", limit=1)
+    print(result)
+    
+    models.Country.objects.all()
+
+
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    django.setup()
+    main()

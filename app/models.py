014c251d640255308af3c78defec0c7aee10c955
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-17 10:43:17 -0600

    got the contries

--- app/models.py
+++ app/models.py
@@ -8,24 +8,15 @@ class Country(models.Model):
 
 
 class Artist(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    disambiguation = models.CharField(max_length=50)
     begin_date = models.DateField()
     end_date = models.DateField()
     ended = models.BooleanField(default=False)
-
-    MALE = 'M'
-    FEMALE = 'F'
-    OTHER = 'O'
-
-    GENDER_CHOICES = ((MALE, 'Male'),
-                      (FEMALE, 'Female'),
-                      (OTHER, 'Other'))
-
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-
-    bio = models.CharField(max_length=500)
-    country = models.ForeignKey(Country)
+    country = models.CharField(max_length="2")
+    # tags = []
+    # rating = []
+    # country = models.ForeignKey(Country)
 
 
 class Track(models.Model):
@@ -36,11 +27,14 @@ class Track(models.Model):
 
 
 class Label(models.Model):
-    country = models.ForeignKey(Country)
     name = models.CharField(max_length=50)
+    label_type = models.CharField(max_length=50)
+    country = models.CharField(max_length="2")
+    # country = models.ForeignKey(Country)
     founded_date = models.DateField()
     dissolved_date = models.DateField()
     dissolved = models.BooleanField(default=False)
+    disambiguation = models.CharField()
 
 
 class Release(models.Model):

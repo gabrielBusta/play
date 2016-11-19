43e910fcddbe0a3035ed3f8479240610898f4ad1
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-18 22:21:41 -0600

    made it so it'll fecth data on command

--- artists.py
+++ artists.py
@@ -6,50 +6,42 @@ import musicbrainzngs as mbz
 
 
 def main():
-    artists = []
-
-    mbz.set_useragent('play music app', '0.1', 'gabrielbusta@gmail.com')
-
-    artists = mbz.search_artists(country='US', limit=100)['artist-list']
-    artists.extend(mbz.search_artists(country='MX', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='VE', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='FR', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='JP', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='KR', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='BR', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='CN', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='GB', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='RU', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='IT', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='ES', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='NG', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='ZA', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='IN', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='CA', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='AU', limit=100)['artist-list'])
-    artists.extend(mbz.search_artists(country='NZ', limit=100)['artist-list'])
-
-    print(len(artists))
-    # write_json(artists, './artists.json')
-    exit(0)
-
-    for key in result.keys():
-        print(key)
-
-    print('number of artist:' + str(len(result['artist-list'])))
-
-    print('artist-list = ')
-
-    uprint(result['artist-list'])
-
-    for artist in result['artist-list']:
-        uprint(artist)
-        print('\n')
-
+    pass
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     import django
     django.setup()
     from app import models
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'fetch':
+            print('fetching artists from musicbrainz.org...')
+            mbz.set_useragent('play music app', '0.1', 'gabrielbusta@gmail.com')
+
+            artists = []
+            artists = mbz.search_artists(country='US', limit=100)['artist-list']
+            artists.extend(mbz.search_artists(country='MX', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='VE', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='FR', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='JP', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='KR', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='BR', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='CN', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='GB', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='RU', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='IT', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='ES', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='NG', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='ZA', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='IN', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='CA', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='AU', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='NZ', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='NO', limit=100)['artist-list'])
+            artists.extend(mbz.search_artists(country='AR', limit=100)['artist-list'])
+
+            print('saving to artists to artists.json...')
+            write_json(artists, './artists.json')
+
     main()

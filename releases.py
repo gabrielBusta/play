83fc86fbf3d4477fe2d0c7785330e8b6c1085ac8
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-20 11:51:34 -0600

    changed to release group

--- releases.py
+++ releases.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) == 2:
         if sys.argv[1] == 'fetch':
-            sys.stdout.write('setting musicbrainz.org user agent... ')
+            sys.stdout.write('Setting up musicbrainz.org user agent... ')
             mbz.set_useragent('academic database project', '0.1', 'abbyyy23@gmail.com')
             sys.stdout.write(Fore.GREEN + 'DONE\n')
 

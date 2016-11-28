831837d16a3d76d56d51202166005dc1edc91b97
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-27 23:16:01 -0600

    added library query

---
+++ sql/library.sql
@@ -0,0 +1,11 @@
+SELECT
+auth_user.username,
+app_recording.title AS song,
+app_recording.length AS time,
+app_album.title AS album,
+app_artist.name AS artist
+FROM auth_user
+JOIN app_profile_recordings ON app_profile_recordings.profile_id = auth_user.id
+JOIN app_recording ON app_recording.id = app_profile_recordings.recording_id
+JOIN app_artist ON app_artist.id = app_recording.artist_id
+JOIN app_album ON app_album.id = app_recording.album_id;

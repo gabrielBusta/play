21267c45c27552fe60bc0f1b8e46ed75b94a3678
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-29 16:07:04 -0600

    formatted aggregate

--- sql/functions/percentage_categories.sql
+++ sql/functions/percentage_categories.sql
@@ -7,15 +7,13 @@ CREATE OR REPLACE FUNCTION public.percentage_categories(OUT group_percent double
 RETURNS record
 AS $function$
 DECLARE
-total double precision;
-num_group double precision;
-num_person double precision;
-num_orchestra double precision;
-num_choir double precision;
-num_character_percent double precision;
-num_other double precision;
-
-
+    total double precision;
+    num_group double precision;
+    num_person double precision;
+    num_orchestra double precision;
+    num_choir double precision;
+    num_character_percent double precision;
+    num_other double precision;
 BEGIN
     SELECT COUNT(*) INTO total FROM app_artist;
     SELECT COUNT(*) INTO num_group FROM app_artist WHERE category = 'Group';
@@ -25,12 +23,11 @@ BEGIN
     SELECT COUNT(*) INTO num_character_percent FROM app_artist WHERE category = 'Character';
     SELECT COUNT(*) INTO num_other FROM app_artist WHERE category = 'Other';
 
-    group_percent= (num_group/total)* 100;
-    person=(num_person/total)*100;
-    orchestra=(num_orchestra/total)*100;
-    choir=(num_choir/total)*100;
-    character_percent=(num_character_percent/total)*100;
-    other=(num_other/total)*100;
-
+    group_percent = (num_group/total) * 100;
+    person = (num_person/total) * 100;
+    orchestra = (num_orchestra/total) * 100;
+    choir = (num_choir/total) * 100;
+    character_percent = (num_character_percent/total) * 100;
+    other = (num_other/total) * 100;
 END;
 $function$ LANGUAGE 'plpgsql' STABLE;

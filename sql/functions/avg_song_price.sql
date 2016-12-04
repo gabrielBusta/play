7e45e29735bb28afc49353c10dba42d828d7e2e8
Abby <abbyyy23@gmail.com>
2016-12-03 19:33:28 -0600

    avg function

--- sql/functions/avg_song_price.sql
+++ sql/functions/avg_song_price.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION public.max_song_price()
+CREATE OR REPLACE FUNCTION public.avg_song_price()
 RETURNS double precision
 AS $function$
 DECLARE
     result double precision;
 BEGIN
-    SELECT max(price) FROM app_recording INTO result;
+    SELECT avg(price) FROM app_recording INTO result;
     RETURN result;
 END;
 $function$ LANGUAGE 'plpgsql' STABLE;
